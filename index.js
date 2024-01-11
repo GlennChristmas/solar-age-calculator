b837b36ee82bc5ddec2b8668fa85d74f743c0746
Glenn Christmas <glenn_christmas@hotmail.co.uk>
2024-01-11 20:52:49 +0000

    add footer content

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ const app = express();
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
+let currentDate = new Date();
+let currentYear = currentDate.getFullYear();
+
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", { currentYear });
 });
 
 const PORT = process.env.PORT || 3000;
