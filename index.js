4ba962951585fb06692306fd6afc91c501aa39ad
Glenn Christmas <glenn_christmas@hotmail.co.uk>
2024-01-12 18:49:11 +0000

    revert me

--- index.js
+++ index.js
@@ -8,14 +8,26 @@ app.use(express.urlencoded({ extended: true })); //this is a slightly newer appr
 let currentDate = new Date();
 let currentYear = currentDate.getFullYear();
 let birthDate = null;
+let planetAge = [4.17, 1.61, 1, 0.53, 0.08, 0.03, 0.01, 0.006];
 
 app.get("/", (req, res) => {
   res.render("index", { currentYear, birthDate });
 });
 
 app.post("/submit-birthdate", (req, res) => {
-  let birthDate = req.body.birthDate;
+  let birthDate = new Date(req.body.birthDate);
   console.log("Birthdate submitted:", birthDate);
+
+  //let ageDiff = Math.abs(currentDate - birthDate);
+  //let earthAge = Math.floor(ageDiff / (1000 * 60 * 60 * 24 * 365.25));
+
+  /*for (let i = 0; i < planetAge.length; i++) {
+    planetAge[i] *= earthAge;
+  }*/
+
+  //console.log(`Your earth age is ${earthAge}.`);
+  //console.log(planetAge);
+
   res.render("index", { currentYear, birthDate });
 });
 
