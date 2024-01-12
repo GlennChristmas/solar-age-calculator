594fb243d722ffa33ca738c3aeaee377ac6f54d8
Glenn Christmas <glenn_christmas@hotmail.co.uk>
2024-01-12 19:38:05 +0000

    calculate and display ages for each planet upon user POST

--- index.js
+++ index.js
@@ -15,20 +15,22 @@ app.get("/", (req, res) => {
 });
 
 app.post("/submit-birthdate", (req, res) => {
-  let birthDate = new Date(req.body.birthDate);
+  let birthDate = req.body.birthDate;
   console.log("Birthdate submitted:", birthDate);
+  let birthDateNumeric = new Date(req.body.birthDate);
 
-  //let ageDiff = Math.abs(currentDate - birthDate);
-  //let earthAge = Math.floor(ageDiff / (1000 * 60 * 60 * 24 * 365.25));
+  let ageDiff = Math.abs(currentDate - birthDateNumeric);
+  let earthAge = Math.floor(ageDiff / (1000 * 60 * 60 * 24 * 365.25));
 
-  /*for (let i = 0; i < planetAge.length; i++) {
+  for (let i = 0; i < planetAge.length; i++) {
     planetAge[i] *= earthAge;
-  }*/
+    planetAge[i] = Math.round(planetAge[i]);
+  }
 
-  //console.log(`Your earth age is ${earthAge}.`);
-  //console.log(planetAge);
+  console.log(`Your earth age is ${earthAge}.`);
+  console.log(planetAge);
 
-  res.render("index", { currentYear, birthDate });
+  res.render("index", { currentYear, birthDate, planetAge });
 });
 
 const PORT = process.env.PORT || 3000;
