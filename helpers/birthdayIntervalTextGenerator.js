15bff06d399524cfaa1816be02163228c3fcc05a
Glenn Christmas <glenn_christmas@hotmail.co.uk>
2024-01-13 18:04:10 +0000

    monoloith commit, apologies

---
+++ helpers/birthdayIntervalTextGenerator.js
@@ -0,0 +1,44 @@
+function birthdayIntervalTextGeneratorHelper(days) {
+  let birthdayInterval, birthdayIntervalMessage;
+
+  if (days < 7) {
+    birthdayInterval = "days";
+    birthdayIntervalMessage = "Better stock up on snacks and party hats!";
+  } else if (days >= 7 && days < 31) {
+    birthdayInterval = "weeks";
+    birthdayIntervalMessage = "Better send the RSVPs out!";
+  } else if (days >= 31 && days < 365) {
+    birthdayInterval = "months";
+    birthdayIntervalMessage = "Relax, you've got time to plan your party!";
+  } else if (days >= 365 && days < 3650) {
+    birthdayInterval = "years";
+    birthdayIntervalMessage =
+      "You've got a while to wait until your next party.";
+  } else if (days > 3650) {
+    birthdayInterval = "over a decade";
+    birthdayIntervalMessage = "A lot could change before your next birthday.";
+  }
+
+  return { birthdayInterval, birthdayIntervalMessage };
+}
+
+//apply the above with map (using birthdayIntervalTextGenerator as a helper function)
+
+function birthdayIntervalTextGenerator(daysArray) {
+  let intervals = [];
+  let messages = [];
+
+  daysArray.forEach((days) => {
+    const { birthdayInterval, birthdayIntervalMessage } =
+      birthdayIntervalTextGeneratorHelper(days);
+    intervals.push(birthdayInterval);
+    messages.push(birthdayIntervalMessage);
+  });
+
+  return { intervals, messages };
+}
+
+module.exports = {
+  birthdayIntervalTextGeneratorHelper,
+  birthdayIntervalTextGenerator,
+};
---
+++ helpers/nextPlanetAgeDaysCalculator.js
@@ -0,0 +1,14 @@
+function nextPlanetAgeDaysCalculator(age, multiples) {
+  const earthDaysPerOrbit = multiples.map((multiple) => 365.25 / multiple);
+
+  let nextAge = age.map((age) => Math.ceil(age));
+  let nextAgeDiff = nextAge.map((nAge, index) => nAge - age[index]);
+
+  let earthDaysToPlanetBirthday = nextAgeDiff.map(
+    (diff, index) => diff * earthDaysPerOrbit[index]
+  );
+
+  return earthDaysToPlanetBirthday;
+}
+
+module.exports = { nextPlanetAgeDaysCalculator };
