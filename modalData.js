219cbfba088b1160b0ad34987759b6bd40259543
Glenn Christmas <glenn_christmas@hotmail.co.uk>
2024-01-15 19:53:00 +0000

    remove unecessary array wrapping

--- modalData.js
+++ modalData.js
@@ -1,72 +1,56 @@
 let modalData = {
-  mercury: [
-    {
-      type: "rocky world",
-      earthFit: 0.056,
-      moons: 0,
-      funFact:
-        "Mercury has the most extreme temperature fluctuations in the Solar System, ranging from -173°C at night to 427°C during the day.",
-    },
-  ],
-  venus: [
-    {
-      type: "rocky world",
-      earthFit: 0.857,
-      moons: 0,
-      funFact:
-        "Venus rotates on its axis in the opposite direction to most planets, meaning the Sun rises in the west and sets in the east.",
-    },
-  ],
-  earth: [
-    {
-      type: "rocky world",
-      funFact:
-        "Earth is the only planet in our Solar System known to support life and has the most liquid water.",
-    },
-  ],
-  mars: [
-    {
-      type: "rocky world",
-      earthFit: 0.151,
-      moons: 2,
-      funFact:
-        "Mars is home to Olympus Mons, the tallest volcano and largest mountain in the Solar System.",
-    },
-  ],
-  jupiter: [
-    {
-      type: "gas giant",
-      earthFit: 1321,
-      moons: 79,
-      funFact:
-        "Jupiter has the Great Red Spot, a gigantic storm larger than Earth that has been raging for hundreds of years.",
-    },
-  ],
-  saturn: [
-    {
-      type: "gas giant",
-      earthFit: 764,
-      moons: 82,
-      funFact:
-        "Saturn's rings are made up of billions of ice and rock particles, and are the most extensive ring system of any planet.",
-    },
-  ],
-  uranus: [
-    {
-      type: "gas giant",
-      earthFit: 63,
-      moons: 27,
-      funFact:
-        "Uranus is unique for its sideways rotation, essentially orbiting the Sun on its side.",
-    },
-  ],
-  neptune: [
-    {
-      type: "gas giant",
-      earthFit: 58,
-      moons: 14,
-      funFact:
-        "Neptune has the fastest winds in the Solar System, reaching speeds of up to 2,100 kilometers per hour.",
-    },
-  ],
+  mercury: {
+    type: "rocky world",
+    earthFit: 0.056,
+    moons: 0,
+    funFact:
+      "Mercury has the most extreme temperature fluctuations in the Solar System, ranging from -173°C at night to 427°C during the day.",
+  },
+  venus: {
+    type: "rocky world",
+    earthFit: 0.857,
+    moons: 0,
+    funFact:
+      "Venus rotates on its axis in the opposite direction to most planets, meaning the Sun rises in the west and sets in the east.",
+  },
+  earth: {
+    type: "rocky world",
+    funFact:
+      "Earth is the only planet in our Solar System known to support life and has the most liquid water.",
+  },
+  mars: {
+    type: "rocky world",
+    earthFit: 0.151,
+    moons: 2,
+    funFact:
+      "Mars is home to Olympus Mons, the tallest volcano and largest mountain in the Solar System.",
+  },
+  jupiter: {
+    type: "gas giant",
+    earthFit: 1321,
+    moons: 79,
+    funFact:
+      "Jupiter has the Great Red Spot, a gigantic storm larger than Earth that has been raging for hundreds of years.",
+  },
+  saturn: {
+    type: "gas giant",
+    earthFit: 764,
+    moons: 82,
+    funFact:
+      "Saturn's rings are made up of billions of ice and rock particles, and are the most extensive ring system of any planet.",
+  },
+  uranus: {
+    type: "gas giant",
+    earthFit: 63,
+    moons: 27,
+    funFact:
+      "Uranus is unique for its sideways rotation, essentially orbiting the Sun on its side.",
+  },
+  neptune: {
+    type: "gas giant",
+    earthFit: 58,
+    moons: 14,
+    funFact:
+      "Neptune has the fastest winds in the Solar System, reaching speeds of up to 2,100 kilometers per hour.",
+  },
 };
