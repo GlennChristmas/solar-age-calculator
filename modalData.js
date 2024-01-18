ce888814cd31c08c723a8a6de1f5533a7a13d1cc
Glenn Christmas <glenn_christmas@hotmail.co.uk>
2024-01-18 19:02:06 +0000

    iteratively populate modal planet info containers

--- modalData.js
+++ modalData.js
@@ -1,52 +1,52 @@
-let modalData = {
-  mercury: {
+const modalData = {
+  Mercury: {
     type: "rocky world",
     earthFit: 0.056,
     moons: 0,
     funFact:
       "Mercury has the most extreme temperature fluctuations in the Solar System, ranging from -173°C at night to 427°C during the day.",
   },
-  venus: {
+  Venus: {
     type: "rocky world",
     earthFit: 0.857,
     moons: 0,
     funFact:
       "Venus rotates on its axis in the opposite direction to most planets, meaning the Sun rises in the west and sets in the east.",
   },
-  earth: {
+  Earth: {
     type: "rocky world",
     funFact:
       "Earth is the only planet in our Solar System known to support life and has the most liquid water.",
   },
-  mars: {
+  Mars: {
     type: "rocky world",
     earthFit: 0.151,
     moons: 2,
     funFact:
       "Mars is home to Olympus Mons, the tallest volcano and largest mountain in the Solar System.",
   },
-  jupiter: {
+  Jupiter: {
     type: "gas giant",
     earthFit: 1321,
     moons: 79,
     funFact:
       "Jupiter has the Great Red Spot, a gigantic storm larger than Earth that has been raging for hundreds of years.",
   },
-  saturn: {
+  Saturn: {
     type: "gas giant",
     earthFit: 764,
     moons: 82,
     funFact:
       "Saturn's rings are made up of billions of ice and rock particles, and are the most extensive ring system of any planet.",
   },
-  uranus: {
+  Uranus: {
     type: "gas giant",
     earthFit: 63,
     moons: 27,
     funFact:
       "Uranus is unique for its sideways rotation, essentially orbiting the Sun on its side.",
   },
-  neptune: {
+  Neptune: {
     type: "gas giant",
     earthFit: 58,
     moons: 14,
