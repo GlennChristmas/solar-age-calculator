563bc6a973eb03018fd45b98ad137078cea23328
Glenn Christmas <glenn_christmas@hotmail.co.uk>
2024-01-20 18:25:26 +0000

    add dismissibility of modals to dom manipulation

--- public/domManipulation.js
+++ public/domManipulation.js
@@ -3,11 +3,19 @@ document.addEventListener("DOMContentLoaded", (event) => {
   const planetModal = document.querySelectorAll(".planet-modal");
   const planetImage = document.querySelectorAll(".planet-image");
   const overlay = document.querySelector(".planet-overlay");
+  const modalExit = document.querySelectorAll(".planet-modal-exit-button");
 
   for (let i = 0; i < planetImage.length; i++) {
     planetImage[i].addEventListener("click", () => {
       planetModal[i].classList.remove("hidden");
       overlay.classList.remove("hidden");
+    });
+  }
+
+  for (let i = 0; i < planetModal.length; i++) {
+    modalExit[i].addEventListener("click", () => {
+      planetModal[i].classList.add("hidden");
+      overlay.classList.add("hidden");
     });
   }
 });
