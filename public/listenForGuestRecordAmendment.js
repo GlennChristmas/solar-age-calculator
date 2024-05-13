cfe66346a376427fb2c3723381ea81e6bf27094d
Glenn Christmas <glenn_christmas@hotmail.co.uk>
2024-05-13 09:14:16 +0100

    add eventListener for every delete record click that returns record UUID to server

---
+++ public/listenForGuestRecordAmendment.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+  const guestRecordEdit = document.querySelectorAll(
+    ".guest-record-button.edit"
+  );
+  const guestRecordDelete = document.querySelectorAll(
+    ".guest-record-button.delete"
+  );
+
+  //listen out for guest record deletions
+  for (let i = 0; i < guestRecordDelete.length; i++) {
+    guestRecordDelete[i].addEventListener("click", (event) => {
+      //extract record id from element and assign to var
+      var recordUUID = event.currentTarget.getAttribute("data-id");
+
+      //in order to send something to the server, it must be in a string format - JSON is often used for this
+      let jsonString = JSON.stringify({ recordUUID: recordUUID });
+
+      //now send a POST request to server with body contents being the jsonString generated above
+      fetch("/delete-guest", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: jsonString,
+      })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+          }
+          return response.json();
+        })
+        .then((data) => {
+          console.log("Success:", data);
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    });
+  }
+});
